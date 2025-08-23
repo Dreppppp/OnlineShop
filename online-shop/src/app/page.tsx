055066bed6c293a63015379d01966c6dcf8608b4
VanyaIvasuk <vanko20111.1@gmail.com>
2025-08-23 07:24:15 +0300

    test deploy

--- online-shop/src/app/page.tsx
+++ online-shop/src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
-
-import { redirect } from 'next/navigation';
-
 export default function Home() {
-  redirect('/homePage');
-  return null
+  return (
+    <main>
+      <h1>Hello, OnlineShop!</h1>
+    </main>
+  );
 }
